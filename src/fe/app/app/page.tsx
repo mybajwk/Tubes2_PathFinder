4ed6873954a73d9a6e9b6742226cccf5227b9ada
FarelW <eeksapi321@gmail.com>
2024-04-21 03:03:52 +0700

    feat: page app swappable input

--- src/fe/app/app/page.tsx
+++ src/fe/app/app/page.tsx
@@ -1,9 +1,30 @@
+"use client"
+import React, { useState } from 'react';
+import { AiOutlineSwap } from "react-icons/ai";
+import InputSearch from "@/components/inputsearch";
 import { title } from "@/components/primitives";
 
 export default function DocsPage() {
-	return (
-		<div>
-			<h1 className={title()}>App</h1>
-		</div>
-	);
+    // State to hold the values of the inputs
+    const [fromValue, setFromValue] = useState('');
+    const [toValue, setToValue] = useState('');
+
+    // Function to swap the values
+    const handleSwap = () => {
+        setFromValue(toValue);
+        setToValue(fromValue);
+    };
+
+    return (
+        <div>
+            <h1 className={title()}>App</h1>
+            <div className="flex gap-10 mt-10 justify-center items-center">
+                <InputSearch key="from" value={fromValue} onChange={(e) => setFromValue(e.target.value)} />
+                <div onClick={handleSwap} className="cursor-pointer">
+                    <AiOutlineSwap className="w-[40px] h-[40px] text-gray-500 hover:text-gray-700"/>
+                </div>
+                <InputSearch key="to" value={toValue} onChange={(e) => setToValue(e.target.value)} />
+            </div>
+        </div>
+    );
 }
