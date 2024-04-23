5381f91fde76eb03f0f76a89733ec65d21ab9192
FarelW <eeksapi321@gmail.com>
2024-04-24 04:02:03 +0700

    feat: add fetch method post to endpoint

--- src/fe/app/app/page.tsx
+++ src/fe/app/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { AiOutlineSwap } from "react-icons/ai";
 import InputSearch from "@/components/inputsearch";
 import { title } from "@/components/primitives";
@@ -7,13 +7,24 @@ import { Switch, Tab, Tabs } from "@nextui-org/react";
 import LoadingButton from '@/components/button';
 import SwitchFilled from '@/components/switch';
 import { useTheme } from 'next-themes';
-import ForceGraph from '@/components/graph';
+import ForceGraph from '@/components/Graph';
+import { request } from 'http';
 
 export default function DocsPage() {
     const [fromValue, setFromValue] = useState('');
     const [toValue, setToValue] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const { theme } = useTheme();
+    const [showRecommendationsFrom, setShowRecommendationsFrom] = useState(false);
+    const [showRecommendationsTo, setShowRecommendationsTo] = useState(false);
+    const [isBfs, setisBfs] = useState('color');
+    const [isMulti, setisMulti] = useState(false);
+
+    const [fromDisplay, setFromDisplay] = useState('');
+    const [toDisplay, setToDisplay] = useState('');
+    const [actualValue, setActualValue] = useState<string>('');
+    const [displayValue, setDisplayValue] = useState<string>('');
+    // console.log(fromDisplay,fromValue,toDisplay,toValue)
 
     const [tabClassNames, setTabClassNames] = useState({
         tabList: "gap-4 border-5 w-full",
@@ -21,6 +32,10 @@ export default function DocsPage() {
         base: "lg:flex md2:hidden sm:flex-[1.5] xl:flex-[1.2] w-full",
         tabContent: theme === 'dark' ? "text-white" : "text-black"
     });
+
+    const handleMulti = () => {
+        setisMulti(!isMulti);
+    };
 
     useEffect(() => {
         setTabClassNames({
@@ -32,15 +47,77 @@ export default function DocsPage() {
     }, [theme]); // Only re-run the effect if theme changes
 
     const handleSwap = () => {
+        [setFromDisplay, setFromValue, setToDisplay, setToValue].forEach(func => func(''));
+        // Logic to swap the actual values
+        const tempValue = fromValue;
         setFromValue(toValue);
-        setToValue(fromValue);
+        setToValue(tempValue);
+        // Logic to swap the display values
+        const tempDisplay = fromDisplay;
+        setFromDisplay(toDisplay);
+        setToDisplay(tempDisplay);
     };
 
-    const handleSearch = () => {
-        setIsLoading(true);
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 2000);
+    const handleSearch = async () => {
+        let url = 'https://localhost:7780/api/bfs'; // The URL to which the request is sent
+    
+        if (isBfs) {
+            url = 'https://localhost:7780/api/bfs'
+        } else {
+            url = 'https://localhost:7780/api/ids'
+        }
+
+        let data = {
+            start: fromValue,
+            end: toValue,
+            is_multi: isMulti
+        };
+
+        if (!toValue) {
+            const formattedDisplay = toDisplay.replace(/ /g, '_');
+            const url = `https://en.wikipedia.org/wiki/${encodeURIComponent(formattedDisplay)}`;
+            data.end = url;
+        }
+
+        if (!fromValue) {
+            const formattedDisplay = fromDisplay.replace(/ /g, '_');
+            const url = `https://en.wikipedia.org/wiki/${encodeURIComponent(formattedDisplay)}`;
+            data.end = url;
+        }
+
+        console.log(data)
+
+
+        try {
+            if (!fromValue || !toValue) {
+                throw new Error('Data not completed');
+            }
+
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const responseData = await response.json();
+            console.log(responseData);
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    };
+
+    const handleSelectionChange = (key: React.Key) => {
+        if (typeof key === 'string') {
+            setisBfs(key);
+        } else {
+            console.error('Unexpected key type:', typeof key);
+        }
     };
 
     const nodes = [
@@ -59,15 +136,40 @@ export default function DocsPage() {
         { source: 'node3', target: 'node4'},
       ];
 
+      const handleInputChange = (
+        setActualValue: Dispatch<SetStateAction<string>>, 
+        setDisplayValue: Dispatch<SetStateAction<string>>
+      ) => (
+        actualValue: string, 
+        displayValue: string
+      ) => {
+        setActualValue(actualValue);
+        setDisplayValue(displayValue);
+      };
+
     return (
         <div className='flex justify-center flex-col items-center'>
             <h1 className={title()}>App</h1>
             <div className="flex gap-10 mt-10 justify-center items-start">
-                <InputSearch key="from" value={fromValue} onChange={(e) => setFromValue(e.target.value)} />
+            <InputSearch
+                    key="from"
+                    displayValue={fromDisplay}
+                    actualValue={fromValue}
+                    onChange={handleInputChange(setFromValue, setFromDisplay)}
+                    showRecommendations={showRecommendationsFrom}
+                    setShowRecommendations={setShowRecommendationsFrom}
+                />
                 <div onClick={handleSwap} className="cursor-pointer">
                     <AiOutlineSwap className="w-[40px] h-[40px] mt-8 text-gray-500 hover:text-gray-700" />
                 </div>
-                <InputSearch key="to" value={toValue} onChange={(e) => setToValue(e.target.value)} />
+                <InputSearch
+                    key="to"
+                    displayValue={toDisplay}
+                    actualValue={toValue}
+                    onChange={handleInputChange(setToValue, setToDisplay)}
+                    showRecommendations={showRecommendationsTo}
+                    setShowRecommendations={setShowRecommendationsTo}
+                />
             </div>
 
            <div className="flex gap-5 py-10 w-[400px]">
@@ -78,19 +180,21 @@ export default function DocsPage() {
                     variant="bordered"
                     color="secondary"
                     classNames={tabClassNames}
+                    selectedKey={isBfs}
+                    onSelectionChange={handleSelectionChange}
                 >
                     <Tab key="color " title="BFS" />
                     <Tab key="texture" title="IDS" />
                 </Tabs>
             </div>
             <div className="flex justify-center items-center">
-            <SwitchFilled  />
+                <SwitchFilled isSelected={isMulti} onToggle={handleMulti} />
             </div>
             <div className="flex justify-center mt-8" >
-            <LoadingButton/>
+                <LoadingButton handleSearch={handleSearch} />
             </div>
             <div className="flex justify-center items-center bg-zinc-800 bg-opacity-90 rounded-3xl mt-8 w-[800px]">
-            <ForceGraph nodes={nodes} links={links} />
+                <ForceGraph nodes={nodes} links={links} />
             </div>
         </div>
     );
--- src/fe/components/switch.tsx
+++ src/fe/components/switch.tsx
@@ -4,23 +4,32 @@ import InfinityIcon from "./iconinfinity";
 import { SunFilledIcon } from "./icons";
 import { useTheme } from "next-themes";
 
-export default function SwitchFilled() {
+interface SwitchFilledProps {
+  isSelected: boolean; 
+  onToggle: () => void;
+}
+
+const SwitchFilled: React.FC<SwitchFilledProps> = ({ isSelected, onToggle }) => {
   const { theme } = useTheme();
   const [wrapperClassNames, setWrapperClassNames] = useState("");
 
   useEffect(() => {
-    // Compute class names based on the theme
+   
     const classes = cn(
       "w-[80px] h-[40px] mr-0",
       theme === 'dark' ? "group-data-[selected=true]:bg-neutral-700 bg-neutral-700" : "group-data-[selected=true]:bg-neutral-200 bg-neutral-200"
     );
     setWrapperClassNames(classes);
-  }, [theme]); // Dependency array includes theme to re-compute on change
+  }, [theme]);
+  
+  const handleToggle = () => onToggle();
 
   return (
     <div className="flex justify-center items-center">
     <Switch
       defaultSelected
+      isSelected={isSelected}
+      onChange={handleToggle}
       size="lg"
       color="default"
       thumbIcon={({ isSelected, className }) =>
@@ -45,3 +54,5 @@ export default function SwitchFilled() {
     </div>
   );
 }
+
+export default SwitchFilled;
