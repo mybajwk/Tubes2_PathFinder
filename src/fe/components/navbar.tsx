dd2c64d0e7775bb91d8447439fb66c30616e9ee1
FarelW <eeksapi321@gmail.com>
2024-04-26 16:25:26 +0700

    fix: navbar

--- src/fe/components/navbar.tsx
+++ src/fe/components/navbar.tsx
@@ -8,7 +8,6 @@ import {
 	NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
 
@@ -20,6 +19,7 @@ import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
+	GithubIcon,
 	HeartFilledIcon,
 	SearchIcon,
 } from "@/components/icons";
@@ -27,37 +27,17 @@ import {
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-	const searchInput = (
-		<Input
-			aria-label="Search"
-			classNames={{
-				inputWrapper: "bg-default-100",
-				input: "text-sm",
-			}}
-			// endContent={
-			// 	<Kbd className="hidden lg:inline-block" keys={["command"]}>
-			// 		K
-			// 	</Kbd>
-			// }
-			labelPlacement="outside"
-			placeholder="Search..."
-			startContent={
-				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-			}
-			type="search"
-		/>
-	);
 
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky">
-			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-				<NavbarBrand as="li" className="gap-3 max-w-fit">
-					<NextLink className="flex justify-start items-center gap-1" href="/">
+			<NavbarContent className="basis-1/2 sm:basis-full" justify="start">
+				<NavbarBrand as="li" className="gap-1 max-w-fit">
+					<NextLink className="flex justify-start items-center" href="/">
 						<Logo />
 						<p className="font-bold text-inherit text-green-500">PathFinder</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+				<ul className="hidden sm:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
 						<NavbarItem key={item.href}>
 							<NextLink
@@ -82,38 +62,43 @@ export const Navbar = () => {
 				<NavbarItem className="hidden sm:flex gap-2">
 					<ThemeSwitch />
 				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-				<NavbarItem className="hidden md:flex">
+				<NavbarItem className="hidden sm:flex">
 					<Button
-            isExternal
+            			isExternal
 						as={Link}
 						className="text-sm font-normal text-default-600 bg-default-100"
-						startContent={<HeartFilledIcon className="text-danger" />}
+						startContent={<GithubIcon/>}
 						variant="flat"
+						href="https://github.com/mybajwk/Tubes2_PathFinder"
 					>
 					</Button>
 				</NavbarItem>
 			</NavbarContent>
 
-			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+			<NavbarContent className="sm:hidden basis-1 pl-2" justify="end">
 				<ThemeSwitch />
+				<NavbarItem className="sm:hidden">
+					<Button
+            			isExternal
+						as={Link}
+						className="text-sm font-normal text-default-600 bg-default-100 hide-below"
+						startContent={<GithubIcon />}
+						variant="flat"
+						href="https://github.com/mybajwk/Tubes2_PathFinder"
+						size="sm"
+					>
+					</Button>
+				</NavbarItem>
 				<NavbarMenuToggle />
 			</NavbarContent>
 
 			<NavbarMenu>
-				{searchInput}
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 					{siteConfig.navMenuItems.map((item, index) => (
 						<NavbarMenuItem key={`${item}-${index}`}>
 							<Link
-								color={
-									index === 2
-										? "primary"
-										: index === siteConfig.navMenuItems.length - 1
-										? "danger"
-										: "foreground"
-								}
-								href="#"
+								color="foreground"
+								href={item.href}
 								size="lg"
 							>
 								{item.label}
--- src/fe/config/site.ts
+++ src/fe/config/site.ts
@@ -19,36 +19,16 @@ export const siteConfig = {
 	],
 	navMenuItems: [
 		{
-			label: "Profile",
-			href: "/profile",
+			label: "Home",
+			href: "/",
 		},
 		{
-			label: "Dashboard",
-			href: "/dashboard",
+			label: "App",
+			href: "/app",
 		},
 		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
+			label: "About",
+			href: "/about",
+		}
 	],
 };
